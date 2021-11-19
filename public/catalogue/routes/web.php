1590b115a155f21d4a2f1af69f91ffe16b49d005
GingErwan <48923105+GingErwan@users.noreply.github.com>
2021-11-19 11:14:23 +0100

    Mise en place du controller
    - Mise en place des controllers en test

--- public/catalogue/routes/web.php
+++ public/catalogue/routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/listeMedias', 'App\Http\Controllers\listeMediasController@getListeMedias');
+Route::get('/{type}/{annee}', 'App\Http\Controllers\listeMediasController@getListeMediasWithParameters');
+
 Route::get('/', function () {
     return 'hello world :-)';
     // return view('welcome');
