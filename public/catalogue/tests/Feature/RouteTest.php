674c392307a736d6ce43bca123aa02c4ef748cba
Vinciane <qlqqlc@outlook.fr>
2021-11-23 09:12:02 +0000

    testRoute

---
+++ public/catalogue/tests/Feature/RouteTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class RouteTest extends TestCase{
+    
+     /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testRoutes()
+    {
+        $appURL = env('APP_URL');
+
+        $urls = [
+            '/categories',
+            '/films',
+            '/oneFilm',
+            'addFilm',
+            'modifyFilm/1'
+        ];
+
+        echo  PHP_EOL;
+
+        foreach ($urls as $url) {
+            $response = $this->get($url);
+            if((int)$response->status() !== 200){
+                echo  $appURL . $url . ' (FAILED) did not return a 200.';
+                $this->assertTrue(false);
+            } else {
+                echo $appURL . $url . ' (success ?)';
+                $this->assertTrue(true);
+            }
+            echo  PHP_EOL;
+        }
+
+    }
+}
