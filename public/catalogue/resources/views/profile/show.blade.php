0362693cd1da5a4cfc16dbd9588b8724fb7d5a63
Vinciane <qlqqlc@outlook.fr>
2021-12-05 17:23:56 +0000

    fix page profil

--- public/catalogue/resources/views/profile/show.blade.php
+++ public/catalogue/resources/views/profile/show.blade.php
@@ -1,10 +1,9 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Profile') }}
-        </h2>
-    </x-slot>
+@extends( 'template' )
 
+@section("content")
+<x-app-layout>
+    <h1 class="text-center">Profil</h1>
+        <hr>
     <div>
         <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
             @if (Laravel\Fortify\Features::canUpdateProfileInformation())
@@ -43,3 +42,4 @@
         </div>
     </div>
 </x-app-layout>
+@endsection
